b5135d0ffa20ee7ba250f41b51e5ea9d626603aa
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-26 21:57:01 +0800

    practice

---
+++ jspractice.js
@@ -0,0 +1,17 @@
+/*
+
+├── 01-variables.js
+├── 02-data-types.js
+├── 03-operators.js
+├── 04-if-else.js
+├── 05-loops.js
+├── 06-functions.js
+├── 07-arrays.js
+├── 08-objects.js
+├── package.json */
+
+let name = "arcel";;
+let age = 19;
+
+console.log(`My name is ${name}`);
+console.log(`And I am ${age} years old`);
