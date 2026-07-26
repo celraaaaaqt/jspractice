4663091531340884ffd37cc4c92ed8f55473105d
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-26 22:52:50 +0800

    practiced variables again'

---
+++ variable.js
@@ -0,0 +1,8 @@
+let name = "Arcel S. Zamora";
+let age = 19;
+
+let points = 100;
+
+console.log(`My name is ${name}`);
+console.log(`I am ${age} years old.`);
+console.log(`with the exam score of ${points}`);
