14ce251782b9210b5154bcee41922abe03dce6c9
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-26 22:55:28 +0800

    added some variables

--- variable.js
+++ variable.js
@@ -1,8 +1,10 @@
 let name = "Arcel S. Zamora";
 let age = 19;
-
+let favSubject = "Programming";
 let points = 100;
 
 console.log(`My name is ${name}`);
 console.log(`I am ${age} years old.`);
+console.log(`My favorite subject is ${favSubject}`);
 console.log(`with the exam score of ${points}`);
+
