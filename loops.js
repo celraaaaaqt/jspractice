449d57900a247250031f7f6892ca2f78bb22f680
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-26 22:49:01 +0800

    solved looping problems

--- loops.js
+++ loops.js
@@ -1,15 +1,27 @@
-for (let i = 0; i<= 10; i++){
-    console.log(i);
-}
+/*
+for (let i = 0; i <= 10; i++){
+    if (i % 2){
+        console.log(`${i} is odd`);
+    } else {
+        console.log(`${i} is even`);
+    } 
+}*/
+
 
 let j = 0;
-do {
-    console.log(j);
+let number = 5;
+
+do {    
+ let table = j * number;
+    console.log(`${number} x ${j} = ${table} `)
     j++;
 } while (j <= 10);
 
+/*
 let k = 0;
 while(k <= 10){
     console.log(k);
     k++;
 }
+    */
+    
