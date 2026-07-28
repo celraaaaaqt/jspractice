ca853c4960a2e3aba256de1fab61964e04dfa62e
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-28 11:45:46 +0800

    relearned functions

---
+++ functions.js
@@ -0,0 +1,38 @@
+/* function greet(name) {
+    console.log(`Hi ${name}`);
+}
+
+greet("Arcel");
+greet("Salvador");
+greet("Zamora"); */
+
+/* function multiplyTable(number) {
+    let i = 1;
+    while (i <= 10){
+        let multiply = number * i;
+        console.log(`${number} x ${i} = ${multiply}`);
+        i++;
+    }
+
+}
+
+multiplyTable(5);
+multiplyTable(8); */
+
+/* function add (num1, num2){
+
+    return num1 + num2;
+}
+    let result = add(5, 10);
+
+console.log(`${result}`);
+ */
+
+
+
+function isEven(number){
+
+       return number % 2 === 0;
+}
+console.log(isEven(5));
+console.log(isEven(8));
