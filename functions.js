a4e3c4a00d744561d3faf2e6adb2963d76e4ea8e
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-30 15:23:36 +0800

    added palindrome

--- functions.js
+++ functions.js
@@ -88,3 +88,14 @@ console.log(countVowels("Celraaaaa")); */
         return reversed;
     }
     console.log(reversedWord("hello")); */
+
+function isPalindrome(word) {
+let reversed = "";
+
+for (let i = word.length - 1; i >= 0; i--){
+reversed += word[i];
+}
+return reversed === word;
+
+}
+console.log(isPalindrome("arcel pogi"));
