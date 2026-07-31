161240a015d4023d46c174095692d5d54b1b0d85
Arcel Zamora <arcelzamora068@gmail.com>
2026-07-31 10:38:19 +0800

    learned arrays

--- arrays.js
+++ arrays.js
@@ -1,8 +1,51 @@
-let games = ["Valorant", "MLBB", "Minecraft"];
+/* let fruits = [ "Apple", "Banana", "Orange" ]; */
 
-console.log(games[0]);
-console.log(games[2]);
+/* console.log(fruits);
 
-games.push("ML");
+let colors = [ "Red", "Blue", "Green"];
 
-console.log(games);
+console.log(colors[1]);
+
+let pets = [ "Dog", "Cat", "Rabbit" ];
+
+pets[1] = "Hamster";
+
+console.log(pets);
+
+let languages = ["HTML", "CSS"];
+languages.push("Javascript");
+console.log(languages);
+
+ let numbers = [10, 20, 30, 40];
+numbers.pop();
+console.log(numbers);
+
+let students = ["John", "Anna", "Mark", "Lisa"];
+console.log(students.length); */
+
+/* let foods = [ "My favorite foods:", "1. Pizza", "2. Burger", "3. Fries" ];
+console.log(foods[0]);
+console.log(foods[1]);
+console.log(foods[2]);
+console.log(foods[3]); */
+
+/* let todo = [];
+todo.push("Study javaScript");
+todo.push("Build Portfolio");
+todo.push("Apply for Jobs");
+
+console.log(todo);
+
+let task = todo.length;
+
+console.log(`You have ${task} tasks.`); */
+
+let scores = [ 85, 90, 78];
+scores.pop();
+scores.push(95);
+
+scores[1] = 92;
+
+
+console.log(scores);
+console.log(scores.length);
