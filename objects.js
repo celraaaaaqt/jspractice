5b19108c29ba5d4daa46ac281c3a49083868cc07
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-03 23:07:49 +0800

    added some objects example

--- objects.js
+++ objects.js
@@ -12,3 +12,19 @@ console.log(bookAuthor);
 
 const bookPages = book.pages;
 console.log(bookPages);
+
+const crushInfos = 
+  {
+    herName: "Tasha",
+    herAge: 19,
+    herFavSubject: "Tourism",
+  };
+
+const herName = crushInfos.herName;
+console.log(herName);
+
+const herAge = crushInfos.herAge;
+console.log(herAge);
+
+const herFavSubject = crushInfos.herFavSubject;
+console.log(herFavSubject);
