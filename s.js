38e6a8302406c27f688ab22a88c9430a398c3bc9
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-09 13:18:13 +0800

    learned spread operator

---
+++ s.js
@@ -0,0 +1,42 @@
+// ? Spread Operator is written with the "..." three dots. ( an ES6 feature ).
+// ? Its basic job is to "spread out" the contents of an array or object.
+
+/* const numbers = [1, 2, 3];
+
+const newNumbers = [...numbers, 4, 5];
+
+console.log(newNumbers);
+
+const fruit = [ "Apple", "Banana" ];
+const vegetables = [ "Carrot", "Potato "];
+
+const combinedFruitAndVegetables = [...fruit, ...vegetables];
+
+console.log(combinedFruitAndVegetables);
+
+const person = {
+    name: "John",
+    age: 21
+};
+
+const newPersonInfo = {
+    ...person,
+    city: "Manila"
+}
+
+console.log(newPersonInfo);
+
+const student = {
+    name: "Alex",
+    age: 20,
+    course: "BSIT"
+};
+
+const newStudentInfo = {
+    ...student,
+    age: 21,
+    year: 2
+};
+
+console.log(newStudentInfo); */
+
