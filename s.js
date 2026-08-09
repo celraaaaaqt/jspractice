25236d427edc1d532ea6bfebe99f4677ed38e78f
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-09 14:20:10 +0800

    learned spread and rest operators

--- s.js
+++ s.js
@@ -40,3 +40,40 @@ const newStudentInfo = {
 
 console.log(newStudentInfo); */
 
+// ? Spread = Expand | Rest = Collect ("Collect the rest of the values.")
+// ? rest operator 
+
+/* function sum(...numbers) {
+
+let total = 0;
+
+    numbers.forEach(numbers => {
+        total += numbers;
+    });
+    return total;
+}
+
+console.log(sum(10, 20, 30));
+
+const numbers = [10, 20, 30, 40, 50];
+
+const destructureNumbers = [ first, second, ...remaining] = numbers;
+console.log(first);
+console.log(second);
+console.log(remaining);
+
+const person = {
+    name: "John",
+    age: 21,
+    city: "Manila",
+    country: "Philippines"
+};
+
+const { name, ...otherInfos} = person;
+console.log(otherInfos);
+
+function showFirst(first, ...others){
+    console.log(first);
+    console.log(others);
+}
+showFirst("Apple", "Banana", "Orange", "Mango"); */
