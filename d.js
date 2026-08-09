5996513e385d8791914c841ade4092eee4a3afc8
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-09 12:32:45 +0800

    learned destructuring

---
+++ d.js
@@ -0,0 +1,48 @@
+// ? Destructuring gives you a shorter way to extract object's properties.
+// ? the important syntax is: const { property1, property2 } = object;
+
+// todo Destructuring exercises
+
+/* const person = {
+    name: "John",
+    age: 21,
+    city: "Manila"
+};
+
+const { name, age } = person;
+console.log(name);
+console.log(age);
+
+const product = {
+    productName: "Laptop",
+    price: 50000,
+    brand: "Acer"
+};
+
+const { productName, price } = product;
+console.log(productName);
+console.log(price);
+
+const students = [
+    { name: "Alex", age: 20 },
+    { name: "Anna", age: 19 },
+    { name: "John", age: 21 }
+];
+
+students.forEach(({ name, age }) => {
+console.log(name);
+console.log(age);
+});
+
+const products = [
+    { name: "Laptop", price: 50000 },
+    { name: "Mouse", price: 800 },
+    { name: "Keyboard", price: 1500 }
+];
+
+const result = products.map(({ name, price }) => {
+    return `${name} - ${price}`
+    
+});
+console.log(result); */
+
