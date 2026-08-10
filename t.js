f6c47db342f194fb0ca90f95d3d81fade9f79a8d
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-10 15:23:53 +0800

    learned datatypes conversion

---
+++ t.js
@@ -0,0 +1,15 @@
+// ? typeof tells you the type of a value.
+
+// todo type conversion exercises.
+
+const price = "1500";
+
+const priceConvert = Number(price) + 500;
+console.log(priceConvert);
+console.log(typeof priceConvert);
+
+const age = 21;
+
+const ageConvert = String(age);
+console.log(typeof ageConvert);
+
