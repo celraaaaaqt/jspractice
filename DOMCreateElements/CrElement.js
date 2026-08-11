3c9cf4eaa96cd2231d98ac1ef3c1579334ecfc5a
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-11 21:46:39 +0800

    added some designs

--- DOMCreateElements/CrElement.js
+++ DOMCreateElements/CrElement.js
@@ -3,5 +3,6 @@ const firstButton = document.querySelector("#firstButton");
 const output = document.querySelector("#output");
 
 firstButton.addEventListener("click", () => {
+output.classList.add("highlight");
 output.textContent = "hi!";
 });
