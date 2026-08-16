8a965654b55e681d46fea55493f0caa176b4e86a
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-16 18:50:33 +0800

    currently learning classList Toggle

---
+++ DOM_Interaction/classList-toggle.js
@@ -0,0 +1,10 @@
+// ? CLASSLIST TOGGLE 
+// ? toggle() combines those ideas:
+
+/* element.classList.toggle("active");
+
+It means:
+
+If "active" isn't there → add it
+If "active" is already there → remove it */
+
