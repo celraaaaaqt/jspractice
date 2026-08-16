1654bd943f35e39e2e5a7a6e84b409b65660bd24
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-16 19:00:07 +0800

    done for today

--- DOM_Interaction/classList-toggle.js
+++ DOM_Interaction/classList-toggle.js
@@ -9,3 +9,9 @@ If "active" isn't there → add it
 If "active" is already there → remove it */
 
 // ! Example 
+const button = document.querySelector("#button");
+const message = document.querySelector("#message");
+
+button.addEventListener("click", () => {
+    message.classList.toggle("active");
+});
